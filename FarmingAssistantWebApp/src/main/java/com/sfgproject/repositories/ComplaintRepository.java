dfe5ecd3b03d15d7581bed647f3b88414aafc934
KeaDTAU <87448943+KeaDTAU@users.noreply.github.com>
2021-07-26 21:50:20 +0200

    Delete FarmingAssistantWebApp directory

--- FarmingAssistantWebApp/src/main/java/com/sfgproject/repositories/ComplaintRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sfgproject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sfgproject.entities.Complaint;
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-	 
-}
